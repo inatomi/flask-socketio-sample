e5e62f66ecf846f5d4efdba68e5447a96c863bb3
inatomi <inatomi@itrade.co.jp>
2017-07-14 13:17:14 +0900

    fixed

---
+++ app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from flask import Flask, render_template, session, request
+from flask_socketio import SocketIO, emit, join_room, leave_room, \
+    close_room, rooms, disconnect
+
+async_mode = None
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+
+def background_thread():
+    while True:
+        count = 0
+        socketio.sleep(10)
+        socketio.emit('response',
+                      {'data': 'Server generated event', 'count': count},
+                      namespace='/test')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', async_mode=socketio.async_mode)
+
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    global thread
+    if thread is None:
+        thread = socketio.start_background_task(target=background_thread)
+    emit('response', {'data': 'Connected'})        
+
+
+
+
+if __name__ == '__main__':
+    socketio.run(app, port=5000,debug=True)
